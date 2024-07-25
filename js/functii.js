ee77a2ac6bd1c07ee2dda70ce46caaa3f4fde593
Imbrisca Filip <on_make@yahoo.com>
2024-07-25 19:14:59 +0300

    Edit Person

--- js/functii.js
+++ js/functii.js
@@ -1,4 +1,3 @@
- const paginationButtons = document.querySelectorAll(".pButton");
 
 //todo functtie ce primeste ca parametru o persoana si returneaza un card
 
@@ -13,12 +12,13 @@ function createCard(person)
     item.innerHTML=`
         <div class="student-details">
         <img class="avatar" src="${person.picture.large}" alt="Profile Picture">
-        <h3>${person.name.first} ${person.name.last}</h3>
-        <span class="email">${person.email}</span>
+        <h3  class="name editableInput">${person.name.first} ${person.name.last}</h3>
+        <span class="email editableInput" >${person.email}</span>
         </div>
         <div class="joined-details">
-        <span class="date">Joined ${person.registered.date}</span>
+        <span class="date editableInput" >Joined ${person.registered.date}</span>
         </div>
+        <button class="editBtn">Edit</button>
     `
 
 
@@ -31,40 +31,150 @@ function createCard(person)
 
 
 
-function attachCardsPersons(persons , personsLength, start){
+function attachCardsPersons(persons){
     let container=document.querySelector(".student-list");
-  
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
- 
-  
-    for(let i=start;i<personsLength;i++){
 
-
+    container.innerHTML="";
+    for(let i=0;i<persons.length;i++){
         container.appendChild(createCard(persons[i]));
     }
 }
 
 
+
+
+
+
+
 //todo:paginatie
 
 
+//todo functie ce primeste :  array nr paginii si n pe pagina si returneaza pagina
 
-console.log(data.length);
 
-paginationButtons.forEach((paginationButton) => {
-         
-    paginationButton.addEventListener("click", () => {
+//[23,43,32,3,34,21,32,43,54,567,213,43,12,32]
+
+
+
+
+
+function  pagination(array,pagina,perPagina){
+
+    let i;
+    let start = perPagina * (pagina - 1) ;
+    let finish = (perPagina * pagina) - 1;
+
+    let aux=[];
     
-        paginationButtons.forEach((paginationButton) => paginationButton.classList.remove("active") )
+    if(start == -1){
+        start = 1;
+    }
 
-        paginationButton.classList.add("active");
+    console.log(start , finish);
 
-        let index = Number(paginationButton.textContent);
-        let dataLength = data.length;                              
+    for(i= start ; i<array.length && i <= finish; i++){
+       aux.push(array[i]);
+    }
 
-        attachCardsPersons(data, index * 9 , (index - 1) * 9);
-    })
-})
+    return aux;
 
+}
+
+
+
+
+
+function createPage(nr){
+    let perPagina=9;
+    let persons=pagination(data,nr,perPagina);
+    attachCardsPersons(persons);
+}
+
+
+function getPersonFromForm(){
+ 
+    const addFName = document.querySelector(".addFirstName").value;
+    const addLName = document.querySelector(".addLastName").value;
+    const addAge = document.querySelector(".addAge").value;
+    const addEmail = document.querySelector(".addEmail").value;
+    const addDate = document.querySelector(".addDate").value;
+    return ({
+        name: {
+          title: "Miss",
+          first: addFName,
+          last: addLName,
+        },
+        email: addEmail,
+        registered: {
+          date: addDate,
+          age: Number(addAge),
+        },
+        picture: {
+          large: "https://randomuser.me/api/portraits/women/25.jpg",
+          medium: "https://randomuser.me/api/portraits/med/women/25.jpg",
+          thumbnail: "https://randomuser.me/api/portraits/thumb/women/25.jpg",
+        },
+      })
+
+}
+
+
+function transformToInput(element){
+     
+ 
+ 
+     const text = element.textContent;
+
+     const input = document.createElement('input');
+     input.type = "text";
+     input.value = text;
+     input.className = 'editable-input';
+     
+     input.setAttribute('data-original-tag', element.tagName.toLowerCase());
+     input.setAttribute('data-original-classes', element.className);
+
+     element.replaceWith(input);
+}
+
+
+function saveEdit(input) {
+    const value = input.value;
+    const originalTag = input.getAttribute('data-original-tag');
+    const originalClasses = input.getAttribute('data-original-classes');
+    const newElement = document.createElement(originalTag);
+    newElement.textContent = value;
+    newElement.setAttribute('data-original-tag', originalTag);
+
+   
+    if (originalClasses) {
+        newElement.className = originalClasses;
+    }
+
+    input.replaceWith(newElement);
+}
+
+
+// paginationButtons.forEach((paginationButton) => {
+         
+//     paginationButton.addEventListener("click", () => {
+    
+//         paginationButtons.forEach((paginationButton) => paginationButton.classList.remove("active") )
+
+//         paginationButton.classList.add("active");
+
+//         let index = Number(paginationButton.textContent);
+//         let dataLength = data.length;                              
+
+//         attachCardsPersons(data, index * 9 , (index - 1) * 9);
+//     })
+// })
+
+// pagination.addEventListener("click" , (event) => {
+
+//    let object = event.target;
+
+//    if(object.classList.contains("pButton")){
+//     console.log("test");
+//    }
+
+// })
---
+++ js/script.js
@@ -0,0 +1,77 @@
+const paginationButtons = document.querySelectorAll(".pButton");
+const container = document.querySelector(".pagination");
+const addBtn = document.querySelector(".addBtn");
+const studentsList = document.querySelector(".student-list");
+
+
+createPage(1);
+
+
+
+container.addEventListener("click", (event) => {
+
+    let object = event.target;
+
+    if(object.classList.contains("pButton")){
+
+        let pagina = Number(object.textContent);
+    
+        createPage(pagina);
+
+        
+    }
+
+})
+
+studentsList.addEventListener("click", (event) => {
+
+    let object = event.target;
+
+    if(object.classList.contains("editBtn")){
+
+        if(object.innerText === "Save"){         
+
+            let objectParent = object.parentElement;
+            let inputs = objectParent.querySelectorAll('.editable-input');
+            inputs.forEach(input => {
+                saveEdit(input);
+            });
+    
+             object.innerText = "Edit";
+            
+        }
+        else{
+
+        object.style.backgroundColor = "blueviolet";
+        object.innerText = "Save";
+
+        let objectParent = object.parentElement;
+
+         let name = objectParent.querySelector("h3");
+         let email = objectParent.querySelector(".email");
+         let date = objectParent.querySelector(".date");
+        
+        transformToInput(name);
+        transformToInput(email);
+        transformToInput(date);
+        }
+
+    }
+
+
+})
+
+
+
+//crud -->create read update and delete
+
+console.log(data[1]);
+
+addBtn.addEventListener("click", () => {
+ 
+    data.push(getPersonFromForm());
+ 
+    console.log(getPersonFromForm());
+
+
+})
